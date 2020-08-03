11bf920fc4954b164f4c1b60d222616004e047eb
lyceris-chan <ghostdrain@outlook.com>
2020-08-03 02:59:51 +0200

    convert to unix line endings and fix retardness hahayes

--- setup_env.sh
+++ setup_env.sh
@@ -1,14 +1,15 @@
+#!/bin/sh
+
 # Setup the compile enviroment with Akhil Narang's setup script
 git clone https://github.com/akhilnarang/scripts/
-
-bash /$pwd/script/setup/android_build_env.sh
-
-rm -rf /$pwd/scripts
-
+sh ./scripts/setup/android_build_env.sh
+cd ..
+rm -rf cscript/scripts
 # Clone the latest proton-clang by Kdrag0n
-
 git clone https://github.com/kdrag0n/proton-clang
-
 # Clone AnyKernel3
-
 git clone https://github.com/Daisy-Q-sources/AnyKernel3
+
+
+
+
