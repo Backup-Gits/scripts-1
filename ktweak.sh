cda4a140dcb0652c3acf2e132ff883720dea4130
lyceris-chan <ghostdrain@outlook.com>
2020-08-21 21:09:45 +0200

    ktweak: lacia stop

--- ktweak.sh
+++ ktweak.sh
@@ -8,9 +8,11 @@ LOCATION=$(curl -s https://api.github.com/repos/Magisk-Modules-Alt-Repo/ktweak/r
 ; curl -L -o ktweakm.zip $LOCATION
 
 # Zip up just the ktweak stuff not the whole release folder (haven't found a better way to do this yet)
+rm -rf ktweakm
 unzip ktweakm.zip -d ./ktweakm
 cd ktweakm
-lsd=$(ls -d */|head -n 1)
+lsd=$(ls -d *|head -n 1)
+rm -rf /home/lacia/keso/AnyKernel3/$lsd
 cp -r $(pwd)/$lsd /home/lacia/keso/AnyKernel3
 cd ..
 echo $lsd > ktweak.version
