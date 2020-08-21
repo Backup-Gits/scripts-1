98804ce004cfbcf483dda23c4d0c953a5d403cbb
lyceris-chan <ghostdrain@outlook.com>
2020-08-21 20:58:56 +0200

    ktweak: lacia ur dumb

--- ktweak.sh
+++ ktweak.sh
@@ -5,15 +5,12 @@ rm -rf ktweak
 LOCATION=$(curl -s https://api.github.com/repos/Magisk-Modules-Alt-Repo/ktweak/releases/latest \
 | grep "tag_name" \
 | awk '{print "https://github.com/Magisk-Modules-Alt-Repo/ktweak/archive/" substr($2, 2, length($2)-3) ".zip"}') \
-; curl -L -o ktweak.zip $LOCATION
+; curl -L -o ktweakm.zip $LOCATION
 
 # Zip up just the ktweak stuff not the whole release folder (haven't found a better way to do this yet)
-unzip ktweak.zip -d ./ktweak
-cd ktweak
+unzip ktweakm.zip -d ./ktweakm
+cd ktweakm
 lsd=$(ls -d */|head -n 1)
-cd $lsd
-zip -r9 ktweak.zip *
+cp -r $(pwd)/$lsd /home/lacia/keso/AnyKernel3
 cd ..
-cd ..
-cp ktweak/$lsd/ktweak.zip .
 echo $lsd > ktweak.version
